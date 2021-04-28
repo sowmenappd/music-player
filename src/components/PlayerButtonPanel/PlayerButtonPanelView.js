1adbf1774a6987db6fcbccc1dab3db306a831f84
Sowmen Rahman <sowmen.appd@gmail.com>
2021-04-29 05:26:01 +0600

    Add volume control support

--- src/components/PlayerButtonPanel/PlayerButtonPanelView.js
+++ src/components/PlayerButtonPanel/PlayerButtonPanelView.js
@@ -8,6 +8,7 @@ import song from "resources/test_sounds/Fade.mp3";
 
 const PlayerButtonPanelView = ({ ...props }) => {
   const [paused, setPaused] = useState(true);
+  const [volume, setVolume] = useState(100);
 
   const handlePaused = (paused) => {
     setPaused(paused);
@@ -15,12 +16,19 @@ const PlayerButtonPanelView = ({ ...props }) => {
     else document.getElementById("player").pause();
   };
 
+  const handleVolumeChange = (v) => {
+    setVolume(v);
+    document.getElementById("player").volume = v / 100;
+  };
+
   return (
     <CustomCard flexDir="column" bg="warning600" {...props}>
-      <audio id="player" src={song} autoPlay={false} />
+      <audio id="player" src={song} autoPlay={false} muted={volume == 0} />
       <ParentPanel
         paused={paused}
+        volume={volume}
         playHandler={() => handlePaused(!paused)}
+        volumeHandler={handleVolumeChange}
         iconSize="28px"
         liked={false}
       />
@@ -28,13 +36,22 @@ const PlayerButtonPanelView = ({ ...props }) => {
   );
 };
 
-const ParentPanel = ({ liked, paused, playHandler, iconSize }) => {
+const ParentPanel = ({
+  liked,
+  paused,
+  volume,
+  playHandler,
+  volumeHandler,
+  iconSize,
+}) => {
   return (
     <>
       <Flex w="90%" align="flex-start" justify="space-around">
         <ControlPanel
           paused={paused}
+          volume={volume}
           playHandler={playHandler}
+          onChangeVolume={volumeHandler}
           size={iconSize}
         />
         {!isMobile() && <MusicInfoPanel />}
--- src/components/PlayerButtonPanel/Subpanels/ControlPanel.js
+++ src/components/PlayerButtonPanel/Subpanels/ControlPanel.js
@@ -1,15 +1,35 @@
+import "react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css";
+import RangeSlider from "react-bootstrap-range-slider";
+
 import { Button, Flex } from "components";
 
-const ControlPanel = ({ paused, size, playHandler }) => (
-  <Flex w="" align="center" justify="flex-start">
-    <Button name="PlayPrev" size={size} />
-    <Button
-      name={!paused ? "Pause" : "Play"}
-      size={size}
-      rounded="circle"
-      onClick={() => playHandler(!paused)}
-    />
-    <Button name="PlayNext" size={size} />
+const ControlPanel = ({
+  paused,
+  volume,
+  size,
+  playHandler,
+  onChangeVolume,
+}) => (
+  <Flex w="" col>
+    <Flex align="center" justify="flex-start">
+      <Button name="PlayPrev" size={size} />
+      <Button
+        name={!paused ? "Pause" : "Play"}
+        size={size}
+        rounded="circle"
+        onClick={() => playHandler(!paused)}
+      />
+      <Button name="PlayNext" size={size} />
+    </Flex>
+    <Flex>
+      <RangeSlider
+        value={volume}
+        tooltip="off"
+        onChange={({ target: { value } }) => {
+          onChangeVolume?.(value);
+        }}
+      />
+    </Flex>
   </Flex>
 );
 
